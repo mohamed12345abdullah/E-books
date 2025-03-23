ae544f8b3766d237d4fe538c2c7c35488e1c6a36
CodeMInds-sys <112867836+mohamed12345abdullah@users.noreply.github.com>
2025-03-23 17:19:25 +0200

    cors

--- middlewares/commonMiddleware.js
+++ middlewares/commonMiddleware.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const setupCommonMiddleware = (app) => {
-    app.use(cors()); // Allow all CORS requests
+    app.use(cors({ origin: "*" })); // Allow all CORS requests
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 };
--- middlewares/jwtMiddleware.js
+++ middlewares/jwtMiddleware.js
@@ -32,20 +32,7 @@ const verifyToken = async (req, res, next) => {
 
 
 
-const testVErifyToken= async (token) => {
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log("decoded",decoded);
-    } catch (error) {
-        console.log("error",error.message);
-        console.log(error);
-    }
 
-
-};
-
-
-testVErifyToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3ZDc0MDI2NjgyZmQ2NWFjMjY1Y2ZlZSIsImlhdCI6MTc0Mjc0MTgzNSwiZXhwIjoxNzQyODI4MjM1fQ.wmEX1m0xetU3HR16F_UymZZp_TcWe_tzXGhCKvYCvVM")
 
 module.exports = {
     generateToken,
